444d44a00dc2d6580c3089a1bd39839679c16a43
Bryan <morganthemosaic@gmail.com>
2023-02-27 18:40:11 -0500

    Solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, target) {
+    for(let i = 1; i < arguments.length; i++){
+        if(array.indexOf(arguments[i]) !== -1){
+            array.splice(array.indexOf(arguments[i]), 1);
+        }
+    }
 
+    return array;
 };
 
 // Do not edit below this line
