1e1157a555a71beb65b4449b33ef0974e1d99b88
Bryan <morganthemosaic@gmail.com>
2023-02-27 18:57:33 -0500

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    if (num1 > 0 && num2 > 0 && typeof num1 === "number" && typeof num2 === "number") {
+        let sum = 0;
+
+        if (num1 < num2) {
+            for (let i = num1; i <= num2; i++) {
+                sum += i;
+            }
+        }
+        else {
+            for (let i = num1; i >= num2; i--) {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+    else {
+        return "ERROR";
+    }
 
 };
 
