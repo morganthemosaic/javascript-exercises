41d625a83d9db9b6633b357c60bc7a1b1fef6766
Bryan <morganthemosaic@gmail.com>
2023-02-26 21:07:18 -0500

    Solve 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, repetitions) {
+    if(repetitions < 0){
+        return "ERROR";
+    }
+    else {
+        let repeatedString = "";
 
+        for(let i = 0; i < repetitions; i++){
+            repeatedString += string;
+        }
+
+        return repeatedString;
+    }
 };
 
 // Do not edit below this line
