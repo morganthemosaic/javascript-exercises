0608ac43c81057b79db49341e172b4a3e8ad9808
Bryan <morganthemosaic@gmail.com>
2023-03-01 21:44:19 -0500

    Solve 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+
+	for(let i = 0; i < array.length; i++){
+		sum += array[i];
+	}
+
+	return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+	let product = 1;
 
+	for(let i = 0; i < array.length; i++){
+		product *= array[i];
+	}
+
+	return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if(a === 0){
+		return 1;
+	}
+	else {
+		return a * factorial(a - 1);
+	}
 };
 
 // Do not edit below this line
