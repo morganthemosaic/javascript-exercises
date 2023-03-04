384a70b4105bc58865eb37b8615930652f22a445
Bryan <morganthemosaic@gmail.com>
2023-03-04 10:35:48 -0500

    Solve for defined birth and death

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldestPerson = {
+        name: null,
+        age: 0,
+    };
 
+    for (const person of people){
+        let age = calculateAge(person);
+        
+        if(age > oldestPerson.age){
+            oldestPerson.age = age;
+            oldestPerson.name = person.name;
+        }
+    }
+
+    return oldestPerson;
 };
+
+const calculateAge = (person) => {
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
