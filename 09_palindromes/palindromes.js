5e8997dd86fb26f5edb67a4208a2a7e989d97f7f
Bryan <morganthemosaic@gmail.com>
2023-03-04 09:44:54 -0500

    Solve for single words

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (originalString) {
+    for(let i = 0; i < originalString.length; i++){
+        if(originalString[i] !== originalString[originalString.length - i - 1]){
+            return false;
+        }
+    }
 
+    return true;
 };
 
 // Do not edit below this line
